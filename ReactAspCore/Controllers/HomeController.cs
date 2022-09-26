1b70d3639c19f6f6d562b87b7bb1ae6ff5381353
chien art <chiennguyen.170901@gmail.com>
2022-09-26 14:35:03 +0700

    'update'

---
+++ ReactAspCore/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ReactAspCore.Models;
+
+namespace ReactDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
+        }
+    }
+}
